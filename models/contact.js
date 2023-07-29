03c8c94e6e4b9662f17ec51fcccf9490af314a8e
Nearh777 <nearh777@gmail.com>
2023-07-29 17:34:09 +0300

    schema and model

---
+++ models/contact.js
@@ -0,0 +1,13 @@
+import { string } from "joi";
+import {Schema, model} from "mongoose";
+
+const contactSchema = new Schema({
+    name: String,
+    email: String,
+    phone: Number,    
+    favorite: False,
+});
+
+const Contact = model("contact", contactSchema);
+
+export default Contact;
